47ecf37c4b8bd5ff21f9a32114011a35e3bc1a3d
Milad Rahmat Abadi <milad.rahmat@gmail.com>
2025-02-28 18:43:09 +0200

    Removed the unnecessary frees

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: mrahmat- <mrahmat-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:16:34 by mrahmat-          #+#    #+#             */
-/*   Updated: 2025/02/27 12:42:24 by mrahmat-         ###   ########.fr       */
+/*   Updated: 2025/02/28 18:26:13 by mrahmat-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,6 @@ void	free_map(t_map *map)
 		split_free(map->map);
 		map->map = NULL;
 	}
-	if (map->textures.north)
-		mlx_delete_texture(map->textures.north);
-	if (map->textures.south)
-		mlx_delete_texture(map->textures.south);
-	if (map->textures.east)
-		mlx_delete_texture(map->textures.east);
-	if (map->textures.west)
-		mlx_delete_texture(map->textures.west);
 }
 
 int	print_error(char *msg)
